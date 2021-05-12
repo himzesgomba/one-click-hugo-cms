395b6e903c9e8b8473020025916655ef7daed9d8
Ágnes Ferenczi <agn.ferenczi@gmail.com>
2021-05-12 14:22:17 +0200

    prob

---
+++ src/js/cms-preview-templates/arak.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ImageGrid from "./components/image_grid";
+
+export default class ArakPreview extends React.Component {
+  render() {
+    const {entry, getAsset} = this.props;
+
+    return <div><section id="arak">
+    <div class="arak-header">
+        <h2>Árak</h2>
+        <p>Kiváló minőséget kínálunk igen kedvező áron!</p>
+    </div>
+    <div class="arak-container">
+        <div class="arak-text">
+            <h4>Óvodai jelek árai:</h4>
+            {(entry.getIn(['data', 'arak_text']) || []).map((arak_text, i) => 
+                            <p key={i}>{arak_text.get('arak') }</p>)}
+        </div>
+        <div class="bemutat-text">
+            <p>Óvodai csoportok számára lehetőséget biztosítunk fajtánként 1-2 db jel vásárlására is, ezesetben a jelek ára 120 Ft/db (3x3 cm).</p>
+
+            <p>A postaköltség a rendelt termékek súlyától függően 500-600 Ft.
+                Fizetés lehetséges banki átutalással (költsége az adott banktól függ), postai utalvánnyal (költsége min. 540 Ft), vagy készpénzben az áru átvételekor.
+                Utánvétes küldemény feladását a Posta drasztikus áremelése (2013.07.01.) miatt nem vállalom!</p>
+
+
+            <p>Szállítási határidő (a fizetési módtól függően) max. 7-10 nap.</p>
+
+            <p>Kereskedők kérjék egyedi árainkat!</p>
+            <a href="/ajanlatkeres" type="button" style="text-decoration: none;">Árajánlatkérés!</a>
+        </div>
+    </div>
+</section>
+    </div>;
+  }
+}
---
+++ src/js/cms-preview-templates/ovodai_m.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ImageGrid from "./components/image_grid";
+
+export default class Ovodai_MPreview extends React.Component {
+  render() {
+    const {entry, getAsset} = this.props;
+
+    return <div><section id="references">
+      <div class="references-header">
+          <h2 class="section-title">Óvodai jelek - M</h2>
+          <hr/>
+          <p>Esztétikus, könnyen kezelhető, jól mosható jelek, fehér selyemre hímezve, 3x3 cm-es, 5x5 cm-es és 10x10 cm-es méretben. Néhány öltéssel felvarrva egyszerűen rögzíthetők bármilyen felsőruházatra, törülközőre, ágyneműre, fésűtartóra, ballagási tarisznyára, stb.</p>
+          <h5>Árainkat ÁFA nem terheli.</h5>
+      </div>
+      </section>
+      <ImageGrid galleries={entry.getIn(["data", "galleries"])} getAsset={getAsset} />
+    </div>;
+  }
+}
